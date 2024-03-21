75da3ae59ba1f0cce8d34eda167d51769ed994df
Alberto Hideyo Noguchi Filho <alberto2611@gmail.com>
2024-03-21 18:54:32 -0300

    add: turned to installable binary #9 (#12)

--- setup.py
+++ setup.py
@@ -6,4 +6,5 @@ setup(
     description="Reward Point System for Dundier Mifflin",
     author="Alberto Noguchi",
     packages=find_packages(),
+    entry_points={"console_scripts": ["dundie=dundie.__main__:main"]},
 )
