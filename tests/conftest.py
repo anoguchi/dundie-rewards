92f8b65564017415089b1ba8a4d0d997283e0040
Alberto Noguchi <alberto2611@gmail.com>
2024-03-25 14:44:37 -0300

    add: finalizers, setup, and teardown to tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import pytest
+
 MARKER = """\
 unit: mark a test as a unit test
 integration: mark a test as an integration test
@@ -11,3 +13,10 @@ def pytest_configure(config):
     for line in MARKER.split("\n"):
         if line.strip():
             config.addinivalue_line("markers", line)
+
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request):  # Injeção de dependência
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield  # Protocolo de generators
--- tests/test_load.py
+++ tests/test_load.py
@@ -1,11 +1,53 @@
+import os
+import uuid
 import pytest
 from dundie.core import load
 from .constants import PEOPLE_FILE
 
 
+def setup_module():  # Hooks
+    print()
+    print("roda antes dos testes desse modulo\n")
+
+
+def teardown_module():  # Hooks
+    print()
+    print("roda depois dos testes desse modulo\n")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def create_new_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("isso é sujeira...")
+    yield
+    file_.remove()
+
+
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load(request):
     """Test function load function."""
+
+    request.addfinalizer(lambda: print("✅"))
+
+    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt"
+    request.addfinalizer(
+        lambda: os.unlink(filepath) if os.path.exists(filepath) else None
+    )
+
+    with open(filepath, "w") as f:
+        f.write("Arquivo indesejado")
+
+    assert len(load(PEOPLE_FILE)) == 2
+    assert load(PEOPLE_FILE)[0][0] == "J"
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load2():
+    """Test function load function."""
+    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as f:
+        f.write("Arquivo indesejado")
+
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == "J"
